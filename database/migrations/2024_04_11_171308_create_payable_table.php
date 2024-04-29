5ed7e145e83e5c656a8c78c461dbbe1dc0e34cbc
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-04-29 21:41:35 +0800

    controllers, models, and migration file

--- database/migrations/2024_04_11_171308_create_payable_table.php
+++ database/migrations/2024_04_11_171308_create_payable_table.php
@@ -6,29 +6,91 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('payable', function (Blueprint $table) {
-            // payable information
-            $table->string('BUR');
-            $table->string('SupplierName');
-            $table->string('EndUser');
-            $table->string('Amount');
-            $table->string('CurrentLocation');
-            $table->string('TermsPayment');
-            $table->string('Remarks');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('payable');
+        Schema::dropIfExists('payables');
+        Schema::dropIfExists('particular');
+        Schema::dropIfExists('suppliers');
+        Schema::dropIfExists('tracking');
+    }
+
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->string('SupplierName')->primary();
+            $table->string('Address');
+            $table->string('ContactNumber');
+            $table->timestamps();
+        });
+
+        Schema::create('payables', function (Blueprint $table) {
+            $table->string('BUR', 11)->primary()->unique();
+            $table->string('SupplierName')->nullable();
+            $table->foreign('SupplierName')->references('SupplierName')->on('suppliers')->onDelete('set null')->onUpdate('cascade');
+            $table->string('EndUser');
+            $table->float('Amount');
+            $table->string('TermsPayment');
+            $table->timestamps();
+        });
+
+        Schema::create('particular', function (Blueprint $table) {
+            $table->integer('ID')->primary();
+            $table->string('BUR', 11);
+            $table->foreign('BUR')->references('BUR')->on('payables')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('ParticularDesc');
+            $table->float('Amount');
+            $table->timestamps();
+        });
+
+        Schema::create('tracking', function (Blueprint $table) {
+            $table->integer('ID')->primary();
+            $table->string('BUR', 11);
+            $table->foreign('BUR')->references('BUR')->on('payables')->onDelete('cascade')->onUpdate('cascade');
+            $table->enum('CurrentLocation', ['Procurement', 'Accounting', 'Treasurer', 'Budgeting', 'OVPF', 'Property']);
+            $table->string('CurrentStatus');
+            $table->date('CurrentDate');
+            $table->timestamp('CurrentTime'); // pwede daw kunin via $yourModel->submitted_at = now(); sa controller end
+            $table->timestamps();
+        });
+
+        /*Schema::create('files', function (Blueprint $table) {
+            $table->integer('ID')->primary();
+            $table->string('BUR', 11);
+            $table->string('BUR')->references('BUR')->on('payables')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('IAR');
+            $table->string('SRIDE');
+            $table->string('RIS');
+            $table->string('DR');
+            $table->string('SI');
+            $table->string('CRPO');
+            $table->string('BURFile');
+            $table->timestamps();
+        });*/
+
+        Schema::create('disbursement', function (Blueprint $table) {
+            $table->integer('No')->primary()->unique();
+            $table->date('Date');
+            $table->enum('ModePayment', ['Check', 'Cash', 'Others']);
+            $table->string('Payee');
+            $table->string('TIN');
+            $table->string('BUR', 11);
+            $table->foreign('BUR')->references('BUR')->on('payables')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('Address');
+            $table->string('RCOffice');
+            $table->string('RCCode');
+            $table->enum('Certified', ['Supporting Documents Complete', 'Supporting Documents Incomplete', 'Missing Documents', 'Others']);
+            $table->string('CertifiedBy');
+            $table->string('CertifiedPosition');
+            $table->enum('Approved', ['Approved', 'Not Approved']);
+            $table->string('ApprovedBy');
+            $table->string('ApprovedPosition');
+            $table->string('CheckNo');
+            $table->date('CheckDate');
+            $table->string('BankName');
+            $table->string('CheckName');
+            $table->string('JEVNo');
+            $table->string('JEVDate');
+            $table->string('ORNo');
+        });
     }
 };
