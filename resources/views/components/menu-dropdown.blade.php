0c2c945adffbd7284b64a4b6be16b9cac7d041af
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-06 17:14:20 +0800

    added user role

--- resources/views/components/menu-dropdown.blade.php
+++ resources/views/components/menu-dropdown.blade.php
@@ -31,13 +31,19 @@
             :aria-expanded="open"
             :aria-controls="$id('dropdown-menu')"
             type="button"
-            class="flex items-center gap-2 bg-white px-5 py-2.5 text-sm leading-4 font-medium font-['Inter'] dark:focus:ring-gray-600 text-gray-500 dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300"
+            class="flex items-center gap-2 bg-white px-5 py-1.5 text-sm leading-4 font-medium font-['Inter'] dark:focus:ring-gray-600 text-gray-500 dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300"
         >
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
                 <path stroke="currentColor" d="M19.8797 9.05015C19.8797 11.7085 17.7247 13.8635 15.0664 13.8635C12.4081 13.8635 10.2531 11.7085 10.2531 9.05015C10.2531 6.39182 12.4081 4.23682 15.0664 4.23682C17.7247 4.23682 19.8797 6.39182 19.8797 9.05015Z" stroke="#434343" stroke-width="2.40667" stroke-linecap="round" stroke-linejoin="round"/>
                 <path stroke="currentColor" d="M15.0664 17.4735C10.4143 17.4735 6.64307 21.2447 6.64307 25.8968H23.4897C23.4897 21.2447 19.7185 17.4735 15.0664 17.4735Z" stroke="#434343" stroke-width="2.40667" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-            Mara Calinao <!-- should be the name of the user || Auth::user()->name if getting the name from the db record -->                                   
+            <div class="flex flex-col items-start font-['Inter']">
+                <div class="text-sm font-semibold">Mara Calinao</div>
+                <div class="text-xs"> Accounting Officer </div>
+            </div>
+            <!-- should be the name of the user || Auth::user()->name if getting the name from the db record --> 
+            
+            <!-- should be the role of the user || Auth::user()->role if getting the role from the db record -->                                  
 
             <!-- Heroicon: chevron-down -->
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
@@ -53,7 +59,7 @@
             x-on:click.outside="close($refs.button)"
             :id="$id('dropdown-menu')"
             style="display: none;"
-            class="absolute left-0 mt-1 w-40 rounded-md bg-white shadow-md font-['Inter']"
+            class="absolute right-3 mt-1 w-40 rounded-md bg-white shadow-md font-['Inter']"
         >  
             <a href="/accounts" class="flex items-center gap-2 w-full border-b-2 first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm font-semibold hover:bg-gray-50 disabled:text-gray-500">
                 My Account
