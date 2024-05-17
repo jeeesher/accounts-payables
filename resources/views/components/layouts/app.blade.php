846d767766b7e059235856ea73741c628e53d984
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-17 11:59:18 +0800

    added ajax

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,11 +11,12 @@
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         
     </head>
     <body>
         @include ('livewire.accounting.accounting-navigation')
-        @include ('livewire.offices.bt-navigation')
+        
         {{ $slot }}
 
         @stack('scripts')
