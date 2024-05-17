396777e5814f569d1b0dbd884fe841c25f43e78b
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-17 17:24:08 +0800

    fixed fetching of particular per bur

--- app/Livewire/Payables.php
+++ app/Livewire/Payables.php
@@ -12,26 +12,26 @@ class Payables extends Component
     public function render()
     {
         $payables = Payable::select('payables.*', 'tracking.*', 'particular.*')
-                ->join('tracking', 'tracking.BUR', '=', 'payables.BUR')
-                ->joinSub(function($query) {
-                    $query->select('BUR', DB::raw('MAX(created_at) as latest_created_at'))
-                        ->from('tracking')
-                        ->groupBy('BUR');
-                }, 'latest_tracking', function($join) {
-                    $join->on('tracking.BUR', '=', 'latest_tracking.BUR')
-                        ->on('tracking.created_at', '=', 'latest_tracking.latest_created_at');
-                })
-                ->join('particular', 'particular.BUR', '=', 'payables.BUR')
-                ->joinSub(function($query) {
-                    $query->select('BUR', DB::raw('MAX(created_at) as latest_created_at'))
-                        ->from('particular')
-                        ->groupBy('BUR');
-                }, 'latest_particular', function($join) {
-                    $join->on('particular.BUR', '=', 'latest_particular.BUR')
-                        ->on('particular.created_at', '=', 'latest_particular.latest_created_at');
-                })
-            ->orderBy('payables.created_at', 'desc')
-            ->get();
+        ->join('tracking', 'tracking.BUR', '=', 'payables.BUR')
+        ->joinSub(function($query) {
+            $query->select('BUR', DB::raw('MIN(id) as min_id'))
+                ->from('tracking')
+                ->groupBy('BUR');
+        }, 'unique_tracking', function($join) {
+            $join->on('tracking.BUR', '=', 'unique_tracking.BUR')
+                ->on('tracking.id', '=', 'unique_tracking.min_id');
+        })
+        ->join('particular', 'particular.BUR', '=', 'payables.BUR')
+        ->joinSub(function($query) {
+            $query->select('BUR', DB::raw('MIN(id) as min_id'))
+                ->from('particular')
+                ->groupBy('BUR');
+        }, 'unique_particular', function($join) {
+            $join->on('particular.BUR', '=', 'unique_particular.BUR')
+                ->on('particular.id', '=', 'unique_particular.min_id');
+        })
+        ->orderBy('payables.created_at', 'desc')
+        ->get();
 
 
         return view('livewire.accounting.payables', compact('payables'));
