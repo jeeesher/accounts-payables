669e6d6ee3538d3fbe658d545027ac35a0d3efcb
insertnull <149681585+insertnull@users.noreply.github.com>
2024-05-21 01:46:40 +0800

    Update accounting-navigation.blade.php

--- resources/views/livewire/accounting/accounting-navigation.blade.php
+++ resources/views/livewire/accounting/accounting-navigation.blade.php
@@ -16,7 +16,17 @@
                 
                 <div class="flex items-center">
                     <!-- Search Bar -->
-
+                    <form class="flex items-center">
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 start-2 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search" class="w-96 h-10 pl-10 py-1 bg-zinc-100 rounded-lg flex items-center grow shrink basis-0 text-neutral-700 border-none text-base font-normal font-['Nunito Sans'] leading-normal ml-2" placeholder="Search here"></input>
+                        </div>
+                    </form>
 
                     <!-- Dropdown Menu -->
                     <div class="flex items-center ms-2">
