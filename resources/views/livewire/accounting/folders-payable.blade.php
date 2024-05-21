4396574198b184ca668b341af4e99b17e5af1454
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-21 23:28:48 +0800

    upload file type option

--- resources/views/livewire/accounting/folders-payable.blade.php
+++ resources/views/livewire/accounting/folders-payable.blade.php
@@ -15,9 +15,77 @@
                         </svg>
                         Delete
                     </button>
+
+                    <!-- Adding Document Modal -->
+                    <div 
+                        x-data = "{ show: false}" 
+                        x-show = "show"
+                        x-on:open-add-document.window="show = true"
+                        x-on:close-add-document.window="show = false"
+                        style="display: none;"
+                        
+                        class="fixed z-50 inset-0"
+                        >
+
+                        <!-- Background -->
+                        <div class="fixed inset-0 bg-zinc-900 opacity-50"></div>
+
+                        <!-- Modal Content -->
+                        <div class="fixed inset-0 bg-white rounded-md p-6 m-auto h-96 w-auto max-w-lg font-['Inter'] overflow-y-auto">
+                            <div class="pb-4 font-semibold text-base text-black dark:text-gray-200 leading-right flex">Add New Document</div>
+
+                            <div action="">
+                                <div class="mb-4">
+                                    <div class="flex items-center justify-center w-full">
+                                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                                </svg>
+                                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                                <p class="text-xs text-gray-500 dark:text-gray-400">PDF, JPEG, PNG, or JPG</p>
+                                            </div>
+                                            <input id="dropzone-file" type="file" class="hidden" />
+                                        </label>
+                                    </div> 
+                                </div>
+                                <div class="mb-2">
+                                    <div class="flex items-center">
+                                        <label for="DocumentType" class="block mb-2 text-sm font-medium text-black dark:text-white">Document Type</label>
+                                        <span class="text-red-500 p-1 pl-3">*</span>
+                                    </div>
+                                    <select id="DocumentType" name="DocumentType" class="bg-white border border-zinc-200 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                        <option disabled selected>Select Document Type</option>    
+                                        <option value="DV_File">Disbursement Voucher</option>
+                                        <option value="IAR_File">Inspection and Acceptance Report</option>
+                                        <option value="SRIDE_File">Supplies Received and Issued Directly to End-User</option>
+                                        <option value="RIS_File">Requisition and Issue Slip</option>
+                                        <option value="DR_File">Delivery Receipt</option>
+                                        <option value="SI_File">Sales Invoice</option>
+                                        <option value="CRPO_File">Checklist of Requirements from Procurement Office</option>
+                                        <option value="BUR_File">BUR</option>
+                                        <option value="Cheque_File">Cheque</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <!-- Add New Document Footer -->
+                            <div class="flex justify-between pt-4">
+                                <!-- Cancel Button -->
+                                <button x-on:click="$dispatch('close-add-document')" type="button" class="inline-flex items-center text-zinc-950 text-sm font-medium font-['Inter'] bg-white border border-zinc-200 shadow hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
+                                    Cancel
+                                </button>
+
+                                <!-- Add Document Button -->
+                                <button type="button" class="inline-flex items-center text-white text-sm font-medium font-['Inter'] bg-[#2D349A] border border-zinc-200 hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
+                                    Add Document
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                         
                     <!-- Attach New Document Button -->
-                    <button type="button" class="inline-flex items-center text-white text-sm font-medium font-['Inter'] bg-[#2D349A] shadow border border-zinc-200 hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
+                    <button x-data x-on:click="$dispatch('open-add-document')" type="button" class="inline-flex items-center text-white text-sm font-medium font-['Inter'] bg-[#2D349A] shadow border border-zinc-200 hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
                         <svg class="me-2 mb-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                             <path stroke="currentColor" d="M3.33203 8.50016H12.6654M7.9987 3.8335V13.1668" stroke="#FAFAFA" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
