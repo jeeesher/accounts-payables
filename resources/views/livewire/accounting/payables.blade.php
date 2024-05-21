31d023b970c16a6b8ac90048f069bd6f51657a8c
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-21 18:55:05 +0800

    removed due year dropdown

--- resources/views/livewire/accounting/payables.blade.php
+++ resources/views/livewire/accounting/payables.blade.php
@@ -11,9 +11,6 @@
                         <!-- Year Filter Dropdown -->
                         <x-filter-payable-year />
 
-                        <!-- Due Status Filter Dropdown-->
-                        <x-filter-due-year />
-
                         <!-- Delete Button -->
                         <x-delete-payable />
                         <button x-data x-on:click="$dispatch('open-delete-payable')" type="button" class="inline-flex items-center text-zinc-950 text-sm font-medium font-['Inter'] bg-white border border-zinc-200 shadow hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
