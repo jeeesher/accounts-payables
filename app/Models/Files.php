9f99c35f22a1ee89bee498db35342632cace7890
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-05-22 07:55:16 +0800

    created relationship for files and payables

--- app/Models/Files.php
+++ app/Models/Files.php
@@ -10,5 +10,16 @@ class Files extends Model
     use HasFactory;
 
     protected $table = 'files';
+    protected $primaryKey = 'BUR';
+    public $incrementing = false;
+    protected $keyType = 'string';
 
+    protected $fillable = [
+        'BUR', 'IAR_File', 'SRIDE_File', 'RIS_File', 'DR_File', 'SI_File', 'CRPO_File', 'BUR_File', 'Cheque_File', 'DV_File'
+    ];
+
+    public function payable()
+    {
+        return $this->belongsTo(Payable::class, 'BUR', 'BUR');
+    }   
 }
