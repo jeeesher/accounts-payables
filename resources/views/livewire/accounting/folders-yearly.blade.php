325888855b93cd59765f4d9b706394a86f40c82c
Ronanne Bulaon <91105080+jeeesher@users.noreply.github.com>
2024-06-04 05:36:01 +0800

    removed delete button

--- resources/views/livewire/accounting/folders-yearly.blade.php
+++ resources/views/livewire/accounting/folders-yearly.blade.php
@@ -15,6 +15,15 @@
                         Back
                     </button>
                         
+                    <!-- Delete Button -->
+                    <x-delete-folder />
+                    <button x-data x-on:click="$dispatch('open-delete-folder')" type="button" class="inline-flex items-center text-zinc-950 text-sm font-medium font-['Inter'] bg-white border border-zinc-200 shadow hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
+                        <svg class="me-2 mb-0.5" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path stroke="currentColor" d="M13.3294 5.50033V4.83366C13.3294 3.90024 13.3294 3.43353 13.1478 3.07701C12.988 2.7634 12.733 2.50844 12.4194 2.34865C12.0629 2.16699 11.5962 2.16699 10.6628 2.16699H9.32943C8.39601 2.16699 7.9293 2.16699 7.57278 2.34865C7.25917 2.50844 7.00421 2.7634 6.84442 3.07701C6.66276 3.43353 6.66276 3.90024 6.66276 4.83366V5.50033M8.32943 10.0837V14.2503M11.6628 10.0837V14.2503M2.49609 5.50033H17.4961M15.8294 5.50033V14.8337C15.8294 16.2338 15.8294 16.9339 15.5569 17.4686C15.3173 17.939 14.9348 18.3215 14.4644 18.5612C13.9296 18.8337 13.2296 18.8337 11.8294 18.8337H8.16276C6.76263 18.8337 6.06256 18.8337 5.52778 18.5612C5.05738 18.3215 4.67493 17.939 4.43524 17.4686C4.16276 16.9339 4.16276 16.2338 4.16276 14.8337V5.50033" stroke="#344054" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Delete
+                    </button>
+                    
                     <!-- Add Folder Button -->
                     <button type="button" class="inline-flex items-center text-white text-sm font-medium font-['Inter'] bg-[#2D349A] shadow border border-zinc-200 hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200 rounded-lg text-sm px-4 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 active:bg-cyan-700">
                         <svg class="me-2 mb-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
@@ -31,15 +40,24 @@
                     <div class="flex flex-wrap w-full mx-auto">
                         <div class="pr-4 mt-3 lg:w-1/3">
                             <div class="relative h-full pt-2 pb-2 text-left">
+
+                            @php
+                            $payable = DB::table('ap_files')
+                            ->where('created_at', '2024' )
+                            ->get();
+                            @endphp
+
+                            @foreach($payable as $pay)
                                 <div class="mb-4">  
                                     <div class="inline-flex items-center justify-start w-full gap-2 px-3 py-6 bg-white border border-gray-200 rounded-md shadow h-9">
                                         <div class="flex items-center justify-start h-5 gap-2 grow shrink basis-0">
-                                            <div class="ml-2 grow shrink basis-0 text-950 text-sm font-medium font-['Inter'] leading-tight">BUR Number</div>
+                                            <div class="ml-2 grow shrink basis-0 text-950 text-sm font-medium font-['Inter'] leading-tight">{{ $pay->BUR }}</div>
                                         </div>
                                         <!-- Dropdown -->
                                         <x-folders-action-yearly />
                                     </div>
                                 </div> 
+                            @endforeach
                                 
                                 <div class="mb-4">  
                                     <div class="inline-flex items-center justify-start w-full gap-2 px-3 py-6 bg-white border border-gray-200 rounded-md shadow h-9">
